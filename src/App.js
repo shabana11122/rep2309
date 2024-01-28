9acc6fffaa826adec7fd47e8218763baab1a26c3
shabana11122 <121670017+shabana11122@users.noreply.github.com>
2024-01-28 12:43:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-<iframe src="https://www.youtube.com/embed/f_vbAtFSEc0?si=TsHy2a7JMIJQO8uN">RRR Trailor</iframe>
-      <img src="https://m.media-amazon.com/images/M/MV5BODUwNDNjYzctODUxNy00ZTA2LWIyYTEtMDc5Y2E5ZjBmNTMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg"></img>
-      <img src="https://baradwajrangan.files.wordpress.com/2022/03/rrr-the-movie.jpg"></img>
-      <p>RRR is an entirely fictitious story incorporating the lives of two real-life Indian revolutionaries, namely Alluri Sitarama Raju and Komaram Bheem, who fought against the British Raj and the Nizam of Hyderabad respectively. Charan plays Rama Raju while Rama Rao plays Komaram Bheem.</p>
-      <iframe src="https://www.youtube.com/embed/AIdMYKIrHFw?si=GuwD0LVMVTAdYLaH">Waltair Veerayya</iframe>
-      <img src="https://th-i.thgim.com/public/incoming/r63n4t/article66373407.ece/alternates/FREE_1200/waltair%20veerayya%20%283%29.jpg"></img>
-      <img src="https://hellosouth.in/wp-content/uploads/2023/01/Fma3ukxacAA1qDx.jpg"></img>
-      <p>Plot. Waltair Veerayya, a notorious fisherman and smuggler from Vizag, needs money to fight a court case. He is approached by CI Seethapathi, who tells him about Solomon Caesar, a notorious drug lord who escaped from RAW's custody after killing multiple police officers.</p>
-    </div>
+    <h1>Git hub account in shabana</h1>
+     </div>
     
   );
 }
