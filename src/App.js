652c63fae5e95a99af9ccf3e8a3be86913b61f0b
shabana11122 <shabanasyed.ui@gmail.com>
2024-01-28 12:39:15 +0530

    added files new

--- src/App.js
+++ src/App.js
@@ -1,97 +1,34 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import { useRef } from "react";
-import moment from 'moment/moment';
-
 
 function App() {
-  // let dateInputRef = useRef();
-  // let dateSpanRef = useRef();
-  // let ageSpanRef = useRef();
-  let dobInputRef = useRef();
-  // let monthsInputRef = useRef();
-
-  
-  // let daysArr=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-  // let monthsArr=["January","February","March","april","May","June","July","Agust","September","October","November","December"]
   return (
     <div className="App">
-      <form>
-        <div>
-          {/* <lable>Batch Start Dte</lable>
-          <input ref={dateInputRef} type="date"></input>
-          <br>
-          </br> */}
-          {/* <lable>Muhurtham</lable>
-          <input ref={dateInputRef} type="datetime-local"></input>*/}
-        {/* </div>
-        <div>
-          <button type="button" onClick={() => {
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
 
-
-
-            console.log(dateInputRef.current.value);
-            let selectedDate = new Date(dateInputRef.current.value);
-
-            console.log(moment(selectedDate).format('MMMM Do YYYY, h:mm:ss a'));
-            console.log(moment(selectedDate).format('dddd'));
-            console.log(moment(selectedDate).format("MMM Do YY"));
-           
-            console.log(moment(selectedDate).add(120, 'days').calendar());
-            
-            
-            
-            console.log(selectedDate.getHours());
-            console.log(selectedDate.getMinutes());
-            console.log(selectedDate.getSeconds());
-            console.log(selectedDate.getMilliseconds());
-
-            console.log(daysArr[selectedDate.getDay()]);
-            console.log(selectedDate.getDate());
-            console.log(monthsArr[selectedDate.getMonth()]);
-            console.log(selectedDate.getFullYear());
-            console.log(selectedDate.getTime());
-
-            let courseDurationInMS = 120 * 24 * 60 * 60 * 1000;
-            let batchStartDateInMS = selectedDate.getTime();
-            let proposedEndDateInMs = batchStartDateInMS + courseDurationInMS;
-
-            let proposedEndDate = new Date(proposedEndDateInMs);
-
-            console.log(proposedEndDate.getHours());
-            console.log(proposedEndDate.getMinutes());
-            console.log(proposedEndDate.getSeconds());
-            console.log(proposedEndDate.getMilliseconds());
-
-            console.log(daysArr[proposedEndDate.getDay()]);
-            proposedEndDate.setDate(25);
-            proposedEndDate.setMonth(4);
-            proposedEndDate.setFullYear(2024);
-            proposedEndDate.setHours(10);
-            console.log(proposedEndDate.getDate());
-            console.log(monthsArr[proposedEndDate.getMonth()]);
-            console.log(proposedEndDate.getFullYear());
-            console.log(proposedEndDate.getTime());
-            
-          }}>calculate</button> */}
-
-          <h1>Age calculater</h1>
-          <label>Choose your date of Birth:</label>
-          <input ref={dobInputRef}type="date"></input>
-        </div>
-        <div>
-          <button type="button" onClick={() => {
-            let dob = new Date(dobInputRef.current.value);
-            let today = new Date();
-            let ageCal = moment.duration(today - dob);
-            let years = ageCal.years();
-            let months = ageCal.days();
-            let days = ageCal.days();
-            console.log(`The age is:${years}years${months}months|${days}days`);
-          }}>Calculate The Age</button>
-        </div>
-      </form>
+<iframe src="https://www.youtube.com/embed/f_vbAtFSEc0?si=TsHy2a7JMIJQO8uN">RRR Trailor</iframe>
+      <img src="https://m.media-amazon.com/images/M/MV5BODUwNDNjYzctODUxNy00ZTA2LWIyYTEtMDc5Y2E5ZjBmNTMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg"></img>
+      <img src="https://baradwajrangan.files.wordpress.com/2022/03/rrr-the-movie.jpg"></img>
+      <p>RRR is an entirely fictitious story incorporating the lives of two real-life Indian revolutionaries, namely Alluri Sitarama Raju and Komaram Bheem, who fought against the British Raj and the Nizam of Hyderabad respectively. Charan plays Rama Raju while Rama Rao plays Komaram Bheem.</p>
+      <iframe src="https://www.youtube.com/embed/AIdMYKIrHFw?si=GuwD0LVMVTAdYLaH">Waltair Veerayya</iframe>
+      <img src="https://th-i.thgim.com/public/incoming/r63n4t/article66373407.ece/alternates/FREE_1200/waltair%20veerayya%20%283%29.jpg"></img>
+      <img src="https://hellosouth.in/wp-content/uploads/2023/01/Fma3ukxacAA1qDx.jpg"></img>
+      <p>Plot. Waltair Veerayya, a notorious fisherman and smuggler from Vizag, needs money to fight a court case. He is approached by CI Seethapathi, who tells him about Solomon Caesar, a notorious drug lord who escaped from RAW's custody after killing multiple police officers.</p>
     </div>
+    
   );
 }
 
